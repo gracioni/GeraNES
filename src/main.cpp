37c3fd7e3d877c00f7d226bfd2d1245e659bd214
Geraldo Racioni Neto <geraldoracioni@gmail.com>
2024-09-04 01:39:59 -0300

    1.4.1

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@ int main(int argc, char* argv[]) {
 
     GeraNESApp app;
 
-    app.create("GeraNES", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_RESIZABLE);
+    app.create(GERANES_NAME, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_RESIZABLE);
 
     app.run();
 
