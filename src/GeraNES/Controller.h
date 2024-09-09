502d96065624a4bb68193a307415513c7c301e2c
Geraldo Racioni Neto <geraldoracioni@gmail.com>
2024-09-09 09:49:52 -0300

    controller

--- src/GeraNES/Controller.h
+++ src/GeraNES/Controller.h
@@ -9,24 +9,24 @@ class Controller
 {
 private:
 
-    uint8_t m_data[8];
-    int m_index;
+    uint8_t m_register;
+    uint8_t m_load;
 
 public:
 
     Controller()
     {
-        m_index = 0;
-        memset(m_data,0,8*sizeof(uint8_t));
+        m_load = 0;
+        m_register = 0;
     }
 
     uint8_t read(bool outputEnabled)
     {
-        uint8_t ret = m_data[m_index];
+        uint8_t ret = (m_register & 1) ? 0x01 : 0x00;
 
         if(outputEnabled) {
-            if(m_index == 8) ret = 0x01;
-            else m_index++;
+            m_register >>= 1;
+            m_register |= 0x80; //shift 1's into register
         }
 
         return ret;
@@ -34,25 +34,26 @@ public:
 
     void write(uint8_t data)
     {
-        if(data&0x01) m_index = 0;
+        if(data&0x01) m_register = m_load;
     }
 
     void setButtonsStatus(bool bA, bool bB, bool bSelect, bool bStart, bool bUp, bool bDown, bool bLeft, bool bRight)
     {
-        m_data[0] = bA ? 0x01 : 0x00;
-        m_data[1] = bB ? 0x01 : 0x00;        
-        m_data[2] = bSelect ? 0x01 : 0x00;
-        m_data[3] = bStart ? 0x01 : 0x00;
-        m_data[4] = bUp ? 0x01 : 0x00;
-        m_data[5] = (bDown && !bUp) ? 0x01 : 0x00;
-        m_data[6] = (bLeft && !bRight) ? 0x01 : 0x00;
-        m_data[7] = bRight ? 0x01 : 0x00;
+        m_load = 0;
+        m_load |= bA ? 1 : 0;
+        m_load |= bB ? (1 << 1) : 0;        
+        m_load |= bSelect ? (1 << 2) : 0;
+        m_load |= bStart ? (1 << 3) : 0;
+        m_load |= bUp ? (1 << 4) : 0;
+        m_load |= (bDown && !bUp) ? (1 << 5) : 0;
+        m_load |= (bLeft && !bRight) ? (1 << 6) : 0;
+        m_load |= bRight ? (1 << 7) : 0;
     }
 
     void serialization(SerializationBase& s)
     {
-        s.array(m_data, 1, 8);
-        SERIALIZEDATA(s, m_index);
+        SERIALIZEDATA(s, m_register);
+        SERIALIZEDATA(s, m_load);
     }
 
 };
