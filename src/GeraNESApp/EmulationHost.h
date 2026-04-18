ac525d537dd82c5f7ddec4b8518cdb6523e37e1b
Geraldo Racioni Neto <geraldoracioni@gmail.com>
2026-04-18 18:16:59 -0300

    web host threadered test 3

--- src/GeraNESApp/EmulationHost.h
+++ src/GeraNESApp/EmulationHost.h
@@ -5,6 +5,7 @@
 #if defined(__EMSCRIPTEN__) && defined(GERANES_WEB_PTHREADS)
 #include "GeraNESApp/ThreadedEmulationHost.h"
 using EmulationHost = ThreadedEmulationHost;
+static_assert(GERANES_WEB_PTHREADS_INITIAL_MEMORY_MB >= 1, "GERANES_WEB_PTHREADS_INITIAL_MEMORY_MB must be positive");
 #elif defined(__EMSCRIPTEN__)
 #include "GeraNESApp/SingleThreadEmulationHost.h"
 using EmulationHost = SingleThreadEmulationHost;
