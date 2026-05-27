765cab220e6552422fa1e6bdf00966e2d458f653
Geraldo Racioni Neto <geraldoracioni@gmail.com>
2026-05-27 02:56:40 -0300

    DeadEndMapper

--- src/GeraNES/Mappers/Mapper247.h
+++ src/GeraNES/Mappers/Mapper247.h
@@ -6,7 +6,7 @@ class Mapper247 : public DeadEndMapper
 {
 public:
     Mapper247(ICartridgeData& cd)
-        : DeadEndMapper(cd, 247, "undocumented mapper stub. No reliable NESdev or Mesen 2 implementation found.")
+        : DeadEndMapper(cd, 247)
     {
     }
 };
